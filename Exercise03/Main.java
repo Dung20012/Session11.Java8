b94431922cb5da1a77b0adafed0bd6ad1c7858ad
Dung20012 <hantho1107@gmail.com>
2025-06-11 11:15:24 +0700

    Add files via upload

---
+++ Exercise03/Main.java
@@ -0,0 +1,36 @@
+package ra.Exercise03;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+//        Tạo danh sách người dùng
+        List<User> users = Arrays.asList(
+                new User("alice", "alice@example.com"),
+                new User("bob", "bob@example.com"),
+                new User("charlie", "charlie@example.com")
+        );
+//        Tìm kiếm ng dùng theo username
+        Optional<User> userOptional = findUserByUsername(users, "bob");
+//        S/d phương thức Optional
+        if (userOptional.isPresent()) {
+            System.out.println("Người dùng tìm thấy: " + userOptional.get());
+        } else {
+            System.out.println("Không tìm thấy người dùng.");
+        }
+//        Sử dụng orElse để cung cấp giá trị mặc định
+        User defaultUser = userOptional.orElse(new User("default", "default@example.com"));
+        System.out.println("Người dùng (mặc định nếu không tìm thấy): " + defaultUser);
+//        Sử dụng ifPresent để thực hiện hành động nếu có giá trị
+        userOptional.ifPresent(user -> System.out.println("Email của người dùng: " + user.getEmail()));
+    }
+//    Pương thức trả về Optional
+private static Optional<User> findUserByUsername(List<User> users, String username){
+        return users.stream()
+                .filter(user ->
+                        user.getUsername().equals(username))
+                .findFirst();
+}
+}
